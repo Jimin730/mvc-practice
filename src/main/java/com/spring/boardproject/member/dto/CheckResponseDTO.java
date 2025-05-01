7a7cd6154fb6938bdc006fdc1f5982d4fd84ec83
Jimin730 <dnddnd120@naver.com>
2025-05-01 16:56:30 +0900

    feat: DTO 추가

---
+++ src/main/java/com/spring/boardproject/member/dto/CheckResponseDTO.java
@@ -0,0 +1,16 @@
+package com.spring.boardproject.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckResponseDTO {
+
+    private boolean isDuplicate;
+
+}
