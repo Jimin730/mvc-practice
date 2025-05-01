3d8d9741bfb89d05b40924e013f8bff497a6853a
Jimin730 <dnddnd120@naver.com>
2025-05-01 16:38:33 +0900

    feat: 검증 로직 추가

--- src/main/java/com/spring/boardproject/member/dto/MemberDTO.java
+++ src/main/java/com/spring/boardproject/member/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.spring.boardproject.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,18 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MemberDTO {
 
+    // @NotBlank : null, "", " " 모두 허용하지 x
+
+    @NotBlank(message = "아이디를 입력해주세요")
     private String loginId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
+
+    @NotBlank(message = "닉네임을 입력해주세요")
     private String nickName;
 
 }
